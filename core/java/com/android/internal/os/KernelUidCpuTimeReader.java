c18f214bd593475a166cb068ae3bf4b3e5b8ca75
LineageOS-MTK <luzejij@binka.me>
2017-09-11 21:06:24 +0300

    Revert "Fix KernelUidCpuTimeReader"
    This reverts commit 454652015d5ad3dc2623b163cfc6c2d21f154be4.

--- core/java/com/android/internal/os/KernelUidCpuTimeReader.java
+++ core/java/com/android/internal/os/KernelUidCpuTimeReader.java
@@ -137,6 +137,8 @@ public class KernelUidCpuTimeReader {
                 mLastSystemTimeUs.put(uid, systemTimeUs);
                 mLastPowerMaUs.put(uid, powerMaUs);
             }
+        } catch (IOException e) {
+            Slog.e(TAG, "Failed to read uid_cputime: " + e.getMessage());
         }
         mLastTimeReadUs = nowUs;
     }
